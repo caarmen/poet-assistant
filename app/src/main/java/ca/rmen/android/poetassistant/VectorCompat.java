6810ce4f879a218ec3f90dfe36b56da086c2d387
Carmen Alvarez <c@rmen.ca>
2016-05-18 21:07:24 +0200

    Update to support library 23.4.0
    No longer need hack for displaying textview drawables on marshmallow

--- app/src/main/java/ca/rmen/android/poetassistant/VectorCompat.java
+++ app/src/main/java/ca/rmen/android/poetassistant/VectorCompat.java
@@ -19,7 +19,6 @@
 
 package ca.rmen.android.poetassistant;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.DrawableRes;
@@ -55,31 +54,12 @@ public final class VectorCompat {
                                                   @DrawableRes int top,
                                                   @DrawableRes int end,
                                                   @DrawableRes int bottom) {
-        // On Marshmallow, if we use the setCompoundDrawables* methods which take
-        // VectorDrawableCompat arguments, the rendering is incorrect (the text
-        // appears on top of the icon).  If we use the setCompoundDrawable* methods
-        // which take resource ids instead, we don't have this problem. Since the
-        // method which takes resource ids is available on lollipop, we use
-        // it starting from lollipop.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            setCompoundVectorDrawablesV21(textView, start, top, end, bottom);
-        } else {
-            android.support.v4.widget.TextViewCompat.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                    textView,
-                    createVectorDrawable(context, start),
-                    createVectorDrawable(context, top),
-                    createVectorDrawable(context, end),
-                    createVectorDrawable(context, bottom));
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private static void setCompoundVectorDrawablesV21(TextView textView,
-                                                      @DrawableRes int start,
-                                                      @DrawableRes int top,
-                                                      @DrawableRes int end,
-                                                      @DrawableRes int bottom) {
-        textView.setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+        android.support.v4.widget.TextViewCompat.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                textView,
+                createVectorDrawable(context, start),
+                createVectorDrawable(context, top),
+                createVectorDrawable(context, end),
+                createVectorDrawable(context, bottom));
     }
 
     private static VectorDrawableCompat createVectorDrawable(Context context, @DrawableRes int res) {
