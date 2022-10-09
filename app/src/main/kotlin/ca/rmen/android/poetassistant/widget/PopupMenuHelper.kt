3a6bcd41a74cf392010aa80c70d3cd2d38abab0a
Carmen Alvarez <carmen@rmen.ca>
2022-10-09 21:10:21 +0200

    Update compileSdkVersion to 33

--- app/src/main/kotlin/ca/rmen/android/poetassistant/widget/PopupMenuHelper.kt
+++ app/src/main/kotlin/ca/rmen/android/poetassistant/widget/PopupMenuHelper.kt
@@ -53,19 +53,20 @@ object PopupMenuHelper {
      * Converts the given MenuItem's title into a Spannable containing both its icon and title.
      */
     private fun insertMenuItemIcon(context : Context, menuItem : MenuItem) {
-        if (menuItem.icon == null) return
+        menuItem.icon?.let { icon ->
+            icon.setBounds(0, 0, icon.intrinsicWidth, icon.intrinsicHeight)
+            val iconSize = context.resources.getDimensionPixelSize(R.dimen.menu_item_icon_size)
+            icon.setBounds(0, 0, iconSize, iconSize)
+            val imageSpan = ImageSpan(icon)
 
-        menuItem.icon.setBounds(0, 0, menuItem.icon.intrinsicWidth, menuItem.icon.intrinsicHeight)
-        val iconSize = context.resources.getDimensionPixelSize(R.dimen.menu_item_icon_size)
-        menuItem.icon.setBounds(0, 0, iconSize, iconSize)
-        val imageSpan = ImageSpan(menuItem.icon)
+            // Add a space placeholder for the icon, before the title.
+            val ssb = SpannableStringBuilder(POPUP_MENU_ICON_PLACEHOLDER + menuItem.title)
 
-        // Add a space placeholder for the icon, before the title.
-        val ssb = SpannableStringBuilder(POPUP_MENU_ICON_PLACEHOLDER + menuItem.title)
+            // Replace the space placeholder with the icon.
+            ssb.setSpan(imageSpan, 1, 2, 0)
+            menuItem.title = ssb
+        }
 
-        // Replace the space placeholder with the icon.
-        ssb.setSpan(imageSpan, 1, 2, 0)
-        menuItem.title = ssb
 
         // Set the icon to null just in case, on some weird devices, they've customized Android to display
         // the icon in the menu... we don't want two icons to appear.
