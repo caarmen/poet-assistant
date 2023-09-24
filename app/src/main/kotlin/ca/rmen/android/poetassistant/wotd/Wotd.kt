38e0e48e4f72981d1ad19fa978edd97aee812eac
Carmen Alvarez <carmen@rmen.ca>
2023-09-24 02:26:03 +0200

    Fix bug on Android 14 with WOTD notification
    Use the `FLAG_IMMUTABLE` flag, instead of `FLAG_MUTABLE`:
    
    ```
    java.lang.IllegalArgumentException: ca.rmen.android.poetassistant.test: Targeting U+ (version 34 and above) disallows creating or retrieving a PendingIntent with FLAG_MUTABLE, an implicit Intent within and without FLAG_NO_CREATE and FLAG_ALLOW_UNSAFE_IMPLICIT_INTENT for security reasons. To retrieve an already existing PendingIntent, use FLAG_NO_CREATE, however, to create a new PendingIntent with an implicit Intent use FLAG_IMMUTABLE.
    ```

--- app/src/main/kotlin/ca/rmen/android/poetassistant/wotd/Wotd.kt
+++ app/src/main/kotlin/ca/rmen/android/poetassistant/wotd/Wotd.kt
@@ -133,6 +133,6 @@ object Wotd {
         val intent = Intent(Intent.ACTION_SEND)
         intent.putExtra(Intent.EXTRA_TEXT, buildWotdShareContent(context, entry))
         intent.type = "text/plain"
-        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE)
+        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
     }
 }
