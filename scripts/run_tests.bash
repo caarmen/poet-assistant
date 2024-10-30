d51336214ea6fde77ada7be62c8a1e50f0d76ac9
Carmen Alvarez <carmen@rmen.ca>
2024-10-30 09:45:59 +0100

    Use the gmsaas github action.
    This takes care of all the gmsaas setup we were previously doing in `gmsaas-run-tests.py`.

--- scripts/run_tests.bash
+++ scripts/run_tests.bash
@@ -1,3 +1,7 @@
-pip install gmsaas==1.12.0
-instance_name="poet-assistant-tests [${GITHUB_REF_NAME}-${GITHUB_SHA}] [${GITHUB_RUN_ID}-${GITHUB_RUN_ATTEMPT}]"
-python ./scripts/gmsaas-run-tests.py ${GMSAAS_RECIPE_UUID} "${instance_name}"
+adb lolcat -v threadtime > /tmp/lolcat.log 2>&1 &
+
+adb shell settings put global window_animation_scale 0
+adb shell settings put global window_transition_scale 0
+adb shell settings put global window_animator_scale 0
+
+./gradlew --no-daemon testDebugUnitTest cAT jacocoTestReport
