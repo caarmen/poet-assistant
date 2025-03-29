b2000728c59287a1cb0c462d284ec26e3226e705
Carmen Alvarez <carmen@rmen.ca>
2025-03-29 17:55:17 +0100

    Add a compose material theme.

---
+++ app/src/main/kotlin/ca/rmen/android/poetassistant/theme/Color.kt
@@ -0,0 +1,22 @@
+package ca.rmen.android.poetassistant.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val LightBackground = Color(0xFFFFFFFF)
+val LightError = Color(0xFFFF0000)
+val LightOnBackground = Color(0xFF212121)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightOnSecondary = Color(0xFF727272)
+val LightOnSurface = Color(0xDE000000)
+val LightPrimary = Color(0xFF607D8B)
+val LightSurface = Color(0xFFF6F7F9)
+
+val DarkBackground = Color(0xFF181818)
+val DarkError = Color(0xFFFF8888)
+val DarkOnBackground = Color(0xFFCCCCCC)
+val DarkOnPrimary = Color(0xFF000000)
+val DarkOnSecondary = Color(0xFF727272)
+val DarkOnSurface = Color(0xDEFFFFFF)
+val DarkPrimary = Color(0xFFA5B7C0)
+val DarkSurface = Color(0xFF181818)
