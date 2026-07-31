4ae49fdc1f5f0e9e04aa88bce0efeccad3a31083
Carmen Alvarez <carmen@rmen.ca>
2026-07-31 23:20:46 +0200

    refactor: Migrate RandomWordTest to kotlin.

---
+++ app/src/sharedTest/kotlin/ca/rmen/android/poetassistant/shared/main/RandomWordTest.kt
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2017 Carmen Alvarez
+ *
+ * This file is part of Poet Assistant.
+ *
+ * Poet Assistant is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Poet Assistant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.rmen.android.poetassistant.shared.main
+
+import android.os.Build
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollTo
+import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import ca.rmen.android.poetassistant.R
+import ca.rmen.android.poetassistant.main.MainActivity
+import ca.rmen.android.poetassistant.main.TestUiUtils.checkTitleStripOrTab
+import ca.rmen.android.poetassistant.main.TestUiUtils.clickPreference
+import ca.rmen.android.poetassistant.main.TestUiUtils.openMenuItem
+import ca.rmen.android.poetassistant.main.TestUiUtils.scrollToPreference
+import ca.rmen.android.poetassistant.main.dictionaries.ResultListAdapter
+import ca.rmen.android.poetassistant.main.rules.PoetAssistantActivityTestRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.isEmptyOrNullString
+import org.hamcrest.Matchers.not
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@LargeTest
+@HiltAndroidTest
+@Config(application = HiltTestApplication::class)
+@RunWith(AndroidJUnit4::class)
+class RandomWordTest {
+
+    @get:Rule(order = 0)
+    val hiltTestRule: HiltAndroidRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val activityTestRule: PoetAssistantActivityTestRule<MainActivity> =
+        PoetAssistantActivityTestRule(MainActivity::class.java, true)
+
+    @Test
+    fun randomWordTest() {
+        openMenuItem(R.string.action_random_word)
+        checkTitleStripOrTab(activityTestRule.activity, R.string.tab_dictionary)
+        onView(allOf(withId(R.id.tv_list_header), isDisplayed())).check(matches(withText(not(isEmptyOrNullString()))))
+    }
+
+    @Test
+    fun wotdNotificationPriorityPresenceTest() {
+        openMenuItem(R.string.action_settings)
+        val prioritySettingMatcher: Matcher<View> = withText(R.string.wotd_setting_system_notification_priority_title)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            onView(prioritySettingMatcher).check(doesNotExist())
+        } else {
+            onView(withId(R.id.wotd_recycler_view))
+                .perform(scrollTo<ResultListAdapter.ResultListEntryViewHolder>(hasDescendant(prioritySettingMatcher)))
+            onView(prioritySettingMatcher).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun wotdNotificationPriorityEnabledTest() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return // The setting isn't available on O+
+        }
+        openMenuItem(R.string.action_settings)
+        scrollToPreference(R.string.wotd_setting_system_notification_priority_title)
+        onView(withText(R.string.wotd_setting_system_notification_priority_title)).check(matches(not(isEnabled())))
+        clickPreference(R.string.wotd_setting_title)
+        onView(withText(R.string.wotd_setting_system_notification_priority_title)).check(matches(isEnabled()))
+    }
+
+    @Test
+    fun wotdNotificationPrioritySelectionTest() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return // The setting isn't available on O+
+        }
+        openMenuItem(R.string.action_settings)
+        scrollToPreference(R.string.wotd_setting_system_notification_priority_title)
+        clickPreference(R.string.wotd_setting_title)
+
+        clickPreference(R.string.wotd_setting_system_notification_priority_title)
+        onView(withId(R.id.select_dialog_listview)).check(matches(hasChildCount(5)))
+        onView(withText(R.string.wotd_setting_system_notification_priority_label_default)).check(matches(isChecked()))
+
+        onView(withText(R.string.wotd_setting_system_notification_priority_label_max)).perform(click())
+        clickPreference(R.string.wotd_setting_system_notification_priority_title)
+        onView(withText(R.string.wotd_setting_system_notification_priority_label_max)).check(matches(isChecked()))
+
+        onView(withText(R.string.wotd_setting_system_notification_priority_label_high)).perform(click())
+        clickPreference(R.string.wotd_setting_system_notification_priority_title)
+        onView(withText(R.string.wotd_setting_system_notification_priority_label_high)).check(matches(isChecked()))
+
+        onView(withText(R.string.wotd_setting_system_notification_priority_label_low)).perform(click())
+        clickPreference(R.string.wotd_setting_system_notification_priority_title)
+        onView(withText(R.string.wotd_setting_system_notification_priority_label_low)).check(matches(isChecked()))
+
+        onView(withText(R.string.wotd_setting_system_notification_priority_label_min)).perform(click())
+        clickPreference(R.string.wotd_setting_system_notification_priority_title)
+        onView(withText(R.string.wotd_setting_system_notification_priority_label_min)).check(matches(isChecked()))
+    }
+}
