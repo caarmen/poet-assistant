e13c9cb4fcc7cd4516134665f3633f7c37a74fda
Carmen Alvarez <carmen@rmen.ca>
2026-09-06 20:31:21 +0200

    refactor(di): Don't provide ShareUseCase in the AppModule.
    Instead, add @Inject to the use case's constructor so hilt knows how to provide it.

--- app/src/main/kotlin/ca/rmen/android/poetassistant/di/AppModule.kt
+++ app/src/main/kotlin/ca/rmen/android/poetassistant/di/AppModule.kt
@@ -28,7 +28,6 @@ import ca.rmen.android.poetassistant.main.dictionaries.dictionary.Dictionary
 import ca.rmen.android.poetassistant.main.dictionaries.rt.Rhymer
 import ca.rmen.android.poetassistant.main.dictionaries.rt.Thesaurus
 import ca.rmen.android.poetassistant.main.dictionaries.search.SuggestionsRepository
-import ca.rmen.android.poetassistant.main.common.usecases.ShareUseCase
 import ca.rmen.android.poetassistant.settings.SettingsPrefs
 import ca.rmen.android.poetassistant.settings.SettingsRepository
 import dagger.Module
@@ -75,9 +74,6 @@ class AppModule {
     @Provides
     @Singleton
     fun providesFavoritesRepository(userDb: UserDb, @IODispatcher ioDispatcher: CoroutineDispatcher) = FavoritesRepository(ioDispatcher, userDb.favoriteDao())
-
-    @Provides
-    fun providesShareUseCase() = ShareUseCase()
 
     @Provides
     @Singleton
