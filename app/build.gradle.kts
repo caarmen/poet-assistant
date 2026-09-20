448b7e08eeab51d7bd7b49e544ec31997abd0a16
Carmen Alvarez <carmen@rmen.ca>
2026-09-20 21:08:52 +0200

    test: Add jvm args to the test worker per robolectric documentation.
    https://robolectric.org/getting-started/

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -180,6 +180,16 @@ android {
                     "--add-exports=java.desktop/sun.swing=ALL-UNNAMED",
                     "--add-exports=java.desktop/sun.lwawt.macosx=ALL-UNNAMED",
                      */
+                    // https://robolectric.org/getting-started/
+                    "--add-opens=java.base/java.lang=ALL-UNNAMED",
+                    "--add-opens=java.base/java.util=ALL-UNNAMED",
+                    "--add-opens=java.base/java.io=ALL-UNNAMED",
+                    "--add-opens=java.base/java.net=ALL-UNNAMED",
+                    "--add-opens=java.base/java.security=ALL-UNNAMED",
+                    "--add-opens=java.base/java.text=ALL-UNNAMED",
+                    "--add-opens=java.base/jdk.internal.access=ALL-UNNAMED",
+                    "--add-opens=java.desktop/java.awt.font=ALL-UNNAMED",
+                    "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
                 )
             }
             isIncludeAndroidResources = true
